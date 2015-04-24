5f1eb7cdaf3756f67393e06a15c9bdd546b9ad8e
Chase Southard <chase.southard@gmail.com>
2015-04-24 19:54:06 -0400

    add hooker button. inspired by @millidavids
    via https://github.com/millidavids/hooker-button-extension

---
+++ hkr_btn.rb
@@ -0,0 +1,33 @@
+require 'bundler/setup'
+Bundler.require
+
+Dotenv.load
+
+base_url = 'https://api.spark.io'
+device_event_path = "/v1/devices/#{ENV['DEVICE_ID']}/events/#{ENV['EVENT_NAME']}"
+full_path = URI.join(base_url, device_event_path)
+puts "listening at: #{full_path}"
+
+EM.run do
+  # GET /v1/devices/:device_id/events/:event_name
+  url = full_path
+  query = {}
+  headers = {'Authorization' => "Bearer #{ENV['ACCESS_TOKEN']}"}
+  source = EventMachine::EventSource.new(url, query, headers)
+
+  source.message do |message|
+    puts message
+  end
+
+  source.on ENV['EVENT_NAME'] do |message|
+    puts 'received button push'
+    `afplay hooker.mp3`
+    puts 'played hooker.mp3'
+  end
+
+  source.error do |error|
+    puts "error #{error}"
+  end
+
+  source.start # Start listening
+end
